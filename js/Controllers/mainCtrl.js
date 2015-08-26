4c54d374a61bbc05a82e27a8b818cee1e4d7e786
Devin Hoole <devinhoole@outlook.com>
2015-08-25 21:52:34 -0600

    I added more ways to be able to see the program work, you can now see jobs when added, and then can see the array combined.

--- js/Controllers/mainCtrl.js
+++ js/Controllers/mainCtrl.js
@@ -2,29 +2,44 @@ var app = angular.module('jobRotation');
 
 // functions used to generate lists then assing them together.
 app.controller('mainCtrl', function($scope){
+
+  $scope.randomize = false;
+
   $scope.addName = function(name) {
     $scope.names.push({name: name, jobs: []});
+    // console.log($scope.names);
     $scope.name = "";
   };
 
   $scope.addJob = function(job){
-    console.log(job);
+    // console.log(job);
+    // $scope.jobs.push(job);
     $scope.jobs.push(job);
+    // console.log($scope.jobs);
     $scope.job = "";
   };
 
-  $scope.names = [];
-  $scope.jobs = [];
-
-  $scope.randomizer = function(arr, arr2){
-    var trackArr = arr.slice(); //makes shallow copy
+  $scope.randomizer = function(arr1, arr2){
+    var trackArr = arr1.slice(); //makes shallow copy
     for(var i = 0; i < arr2.length; i++){
-      if(!trackArr.length) trackArr = arr.slice();
-
+      if (!trackArr.length) {
+        trackArr = arr1.slice();
+      }
       var num = Math.floor(Math.random() * trackArr.length);
       trackArr[num].jobs.push(arr2[i]);
       trackArr.splice(num, 1);
     }
-    $scope.jobs = [];
+
+    for (var i = 0; i < $scope.jobs.length; i++) {
+      $scope.oldJobs.push($scope.jobs[i]);
+    }
+
+    $scope.jobs = []; //allows you to contiune to add to the list of jobs
+    $scope.randomize = true;
   }
+
+  $scope.names = [];
+  $scope.jobs = [];
+  $scope.oldJobs = [];
+
 });
